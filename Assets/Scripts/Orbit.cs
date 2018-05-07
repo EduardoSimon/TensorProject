ba0639acd83c05e87c924e5432803be3db90d98e
Tsung101 <al341531@uji.es>
2018-05-07 12:54:45 +0200

    Añadidos perdidos Oliver

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+    public float radius;
+    public float yOffset;
+    public Transform centerPoint;
+    public float rotSpeed;
+
+    float timer = 0;
+
+
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime * rotSpeed;
+        Rotate();
+        transform.LookAt(centerPoint);
+    }
+
+    void Rotate() {
+
+        float x = -Mathf.Cos(timer) * radius;
+        float z = Mathf.Sin(timer) * radius;
+        Vector3 pos = new Vector3(x, yOffset, z);
+        transform.position = pos + centerPoint.position;
+    }
+}
